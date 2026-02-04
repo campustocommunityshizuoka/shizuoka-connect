ad359938db85a02cc47e09fa03d60a92817fc819
rai <campustocommunityshizuoka@gmail.com>
2026-02-04 23:32:25 +0900

    fix: partners

--- shizuoka-connect-web/src/app/partners/page.tsx
+++ shizuoka-connect-web/src/app/partners/page.tsx
@@ -30,41 +30,6 @@ export default function PartnersPage() {
             url="https://www.grow-up-academy.com/"
             images={['/assets/growuplogo.jpg']}
           />
-
-          {/* テガラ株式会社 */}
-          <PartnerCard 
-            name="テガラ株式会社"
-            description="研究者・開発者のためのさまざまなサービスを提供している会社です。大学などにおける研究では欠かせない存在です。現在、エンジニア職体験など体験会の募集を行っています。"
-            url="https://www.tegara.com/"
-            images={[
-                '/assets/tegara-logo.png',
-                '/assets/tegara1.JPG',
-                '/assets/tegara2.jpg',
-                '/assets/tegara3.JPG',
-                '/assets/tegara4.JPG'
-            ]}
-          />
-
-          {/* Career Vision Lab */}
-          <PartnerCard 
-            name="一般社団法人Career Vision Lab"
-            description="「ツマラナイ就活を、オモシロく。」をスローガンに、従来の堅苦しい就職活動とは異なるアプローチを特徴. 就活生×社会人 カジュアルコミュニティ 'STAND UP!!'"
-            url="https://share.google/J2gCuAwfwO09tOpGs" // 頂いたコードのままですがリンク切れの可能性あり
-            images={[
-                '/assets/careervision.jpg',
-                '/assets/ca1.JPG',
-                '/assets/ca2.JPG',
-                '/assets/ca3.JPG'
-            ]}
-          />
-
-          {/* 愛管株式会社 */}
-          <PartnerCard 
-            name="愛管株式会社"
-            description="管工事を中心に多くの事業を展開しており，配管工事と付随する企画、設計、施工を40年以上続け、安心安全な生活を提供しています．"
-            url="https://i-kan.co.jp/"
-            images={['/assets/ikan.jpg']}
-          />
         </div>
       </main>
     </>
